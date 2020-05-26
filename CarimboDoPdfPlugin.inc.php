c965c7ac5f698fcb57e72e744fb4f0bae6ef826d
ada <ada@lepidus.com.br>
2020-05-26 16:47:44 -0400

    Adiciona uma página em branco em um pdf com o pdfcpu.
    Signed-off-by: Ada <ada@lepidus.com.br>
    Signed-off-by: Pablo <pablo@lepidus.com.br>

--- CarimboDoPdfPlugin.inc.php
+++ CarimboDoPdfPlugin.inc.php
@@ -7,7 +7,6 @@ class CarimboDoPdfPlugin extends GenericPlugin {
 	public function register($category, $path, $mainContextId = NULL) {
 		$success = parent::register($category, $path);
 		if ($success && $this->getEnabled()) {
-			error_log('plugin registrado');		
 			// Display the publication statement on the article details page
 			HookRegistry::register('SubmissionHandler::saveSubmit', [$this, 'carimbarPdf']);
 		}
@@ -26,15 +25,23 @@ class CarimboDoPdfPlugin extends GenericPlugin {
 		error_log($nomeDoGancho);		
 		
 		$passo = $args[0];
+
 		error_log('carimbarPdf no passo ' . $passo);
+
 
 		if ($passo == 2) {
 			$submissão = $args[1];
 			$arquivos = $submissão->getGalleys();
 			$arquivo = $arquivos[0]->getFile();
-			$caminhoDoPdf = $arquivo->getFilePath();
+			$caminhoDoPdf = $arquivo->getFilePath(); 
 
 			error_log('já posso pegar o arquivo da submissão de tipo: ' . $arquivo->getFilePath());
+			
+			$path = "plugins/generic/carimbo-do-pdf/pdfcpu/";
+
+			error_log(shell_exec($path .'pdfcpu pages insert -pages 1 -mode before '.$arquivo->getFilePath()));
+			
 		}
+
 	}
 }
