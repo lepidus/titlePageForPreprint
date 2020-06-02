b86f08ef8bce4038e5b1468e3757b6202e992406
ada <ada@lepidus.com.br>
2020-06-02 16:37:15 -0400

    Testa se o arquivo original do pdf está mesmo inserido com a folha de rosto - #40
    Após a concatenação da folha de rosto, transformamos o arquivo original e o novo gerado com a folha de rosto em imagens \
    por meio do Imagick e depois comparamos a diferença deles com a mesma biblioteca.
    
    Signed-off-by: Ada <ada@lepidus.com.br>
    Signed-off-by: Pablo <pablo@lepidus.com.br>

--- testes/FolhaDeRostoTest.php
+++ testes/FolhaDeRostoTest.php
@@ -27,6 +27,19 @@ final class FolhaDeRostoTest extends TestCase {
         return new FolhaDeRosto($this->status, $this->doi, $this->logo, $this->checklist);
     }
 
+    private function obterDiferençaImagens(string $caminho1, string $caminho2, int $tipo): array{
+        $imagem1 = new imagick($caminho1);
+        $imagem2 = new imagick($caminho2);
+
+        if($tipo == 1){
+            $imagem1->setImageFormat('jpeg');  
+            $imagem1->writeImage('imagem_pdf_original.jpg');
+            $imagem2->setImageFormat('jpeg');    
+            $imagem2->writeImage('imagem_pdf_folhaderosto.jpg'); 
+        }
+        return $imagem1->compareImages($imagem2, Imagick::METRIC_MEANSQUAREERROR);
+    }
+
     public function testeTemStatusDeSubmissão(): void {
         $folhaDeRosto = $this->obterFolhaDeRostoParaTeste();
         $this->assertEquals($this->status, $folhaDeRosto->obterStatusDeSubmissão());
@@ -103,13 +116,19 @@ final class FolhaDeRostoTest extends TestCase {
         
         $caminhoDaImageExtraida = "testes" . DIRECTORY_SEPARATOR;
         $resultado = shell_exec("pdfimages -f 1 -png ". $pdf->obterCaminho() . " " . $caminhoDaImageExtraida);
-        $imagemExtraida = $caminhoDaImageExtraida . DIRECTORY_SEPARATOR . "-000.png";
-        
-        $imagemExtraidaDoPDF = new imagick($imagemExtraida);
-        $imagemDaLogo = new imagick($this->logo);
-        $diferenca = $imagemDaLogo->compareImages($imagemExtraidaDoPDF, Imagick::METRIC_MEANSQUAREERROR);
+        $imagemExtraida = $caminhoDaImageExtraida . DIRECTORY_SEPARATOR . "-000.png";        
+        $diferenca = $this->obterDiferençaImagens($this->logo, $imagemExtraida, 0);
         $this->assertSame(0.0, $diferenca[1]);
         unlink($imagemExtraida);
     }    
+
+    public function testeInserçãoEmPdfExistentePdfOriginal(): void{
+        $folhaDeRosto = $this->obterFolhaDeRostoParaTeste();
+        $pdfNovo = new Pdf($this->caminhoDoPdfTeste);
+        $folhaDeRosto->inserir($pdfNovo);
+        $pdfOriginal = new Pdf($this->cópiaDoPdfTesteParaRestaurar);
+        $diferenca = $this->obterDiferençaImagens($pdfOriginal->obterCaminho().'[0]', $pdfNovo->obterCaminho().'[1]', 1);
+        $this->assertSame(0.0, $diferenca[1]);
+    }
 }
 ?>
