737027ac4f747853a5ae29eae9992990bec8d6c6
ada <ada@lepidus.com.br>
2020-06-03 17:04:54 -0400

    Obtem o doi e o status da submissão na classe principal do plugin. Relacionado a: https://gitlab.lepidus.com.br/documentacao-e-tarefas/scielo/-/issues/40
    Signed-off-by: Ada <ada@lepidus.com.br>
    Signed-off-by: Pablo <pablo@lepidus.com.br>

--- FolhaDeRostoPlugin.inc.php
+++ FolhaDeRostoPlugin.inc.php
@@ -29,20 +29,40 @@ class FolhaDeRostoPlugin extends GenericPlugin {
 
 		error_log('inserida folha de rosto no passo ' . $passo);
 
+		$submissão = $args[1];
 
 		if ($passo == 2) {
-			$submissão = $args[1];
 			$arquivos = $submissão->getGalleys();
+			$doi = $submissão->getStoredPubId('doi');
+			$status = $submissão->getStatusKey();
 			
 			foreach ($arquivos as $arquivo) {
 				$documento = $arquivo->getFile();
 				$caminhoDoPdf = $documento->getFilePath(); 
-				
-				$pdf = new PDF($caminhoDoPdf);
-				//inserção
-				//carimbação
-				
+				$locale = $arquivo->getLocale();
 			}
+			$checklist = $submissão->getLocalizedData('submissionChecklist'); //se conseguir pegar o context dá pra usar esse método
+			error_log('Doi '. $doi);
+			error_log('Status '. $status);
+			error_log('Locale '. $locale);
+			error_log('Checklist'. $checklist[0]);			
+		}
+		if($passo == 3){
+			$arquivos = $submissão->getGalleys();
+			$doi = $submissão->getStoredPubId('doi');
+			$status = $submissão->getStatusKey();
+			
+			foreach ($arquivos as $arquivo) {
+				$documento = $arquivo->getFile();
+				$caminhoDoPdf = $documento->getFilePath(); 
+				$locale = $arquivo->getLocale();
+			}
+			$checklist = $submissão->getLocalizedData('submissionChecklist');
+			error_log('Doi '. $doi);
+			error_log('Status '. $status);
+			error_log('Locale '. $locale);
+			error_log('Checklist'. $checklist);
+
 
 		}
 
