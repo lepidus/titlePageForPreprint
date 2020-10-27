a51f74bfb8a7fea4f9b116ca63ea214592f81602
Jhon <jhon@lepidus.com.br>
2020-10-27 10:40:13 -0400

    Transforma a fonte de escrita em constante
    Mudança é fruto de sugestão levantada durante a revisão
    
    Signed-off-by: Jhon <jhon@lepidus.com.br>

--- fontes/FolhaDeRosto.inc.php
+++ fontes/FolhaDeRosto.inc.php
@@ -8,6 +8,7 @@ class FolhaDeRosto {
     private $logo;
     private $locale;
     private $tradutor;
+    private $fontName;
     const DIRETORIO_DE_SAIDA = DIRECTORY_SEPARATOR . "tmp" .  DIRECTORY_SEPARATOR;
 
     public function __construct(Submissao $submissão, string $logo, string $locale, tradutor $tradutor) {
@@ -15,6 +16,7 @@ class FolhaDeRosto {
         $this->logo = $logo;
         $this->locale = $locale;
         $this->tradutor = $tradutor;
+        $this->fontName = TCPDF_FONTS::addTTFfont(__DIR__.'/../recursos/opensans.ttf', 'TrueTypeUnicode', '', 32);
     }
 
     public function obterLogo(): string {
@@ -28,9 +30,8 @@ class FolhaDeRosto {
         $folhaDeRosto->AddPage();
         $folhaDeRosto->Image($this->logo, '', '', '35', '20', 'PNG', 'false', 'C', true, 400, 'C', false, false, 0, false, false, false);
         $folhaDeRosto->Ln(25);
-        $fontname = TCPDF_FONTS::addTTFfont(__DIR__.'/../recursos/opensans.ttf', 'TrueTypeUnicode', '', 32);
         
-        $folhaDeRosto->SetFont($fontname, '', 10, '', false);
+        $folhaDeRosto->SetFont($this->fontName, '', 10, '', false);
         $folhaDeRosto->Write(0, $this->tradutor->traduzir('common.status', $this->locale) . ": " . $this->tradutor->traduzir($this->submissão->obterStatus(), $this->locale), '', 0, 'JUSTIFY', true, 0, false, false, 0);
         if($this->submissão->obterStatus() == 'publication.relation.published'){
             $folhaDeRosto->Write(0, $this->tradutor->traduzir('publication.relation.vorDoi', $this->locale) . ": ", '', 0, 'JUSTIFY', false, 0, false, false, 0);
@@ -38,16 +39,16 @@ class FolhaDeRosto {
         }
         $folhaDeRosto->Ln(5);
         
-        $folhaDeRosto->SetFont($fontname, '', 18, '', false);
+        $folhaDeRosto->SetFont($this->fontName, '', 18, '', false);
         $folhaDeRosto->Write(0, $this->tradutor->obterTítuloTraduzido($this->locale), '', 0, 'C', true, 0, false, false, 0);
-        $folhaDeRosto->SetFont($fontname, '', 12, '', false);
+        $folhaDeRosto->SetFont($this->fontName, '', 12, '', false);
         $folhaDeRosto->Write(0, $this->submissão->obterAutores(), '', 0, 'C', true, 0, false, false, 0);
-        $folhaDeRosto->SetFont($fontname, '', 11, '', false);
+        $folhaDeRosto->SetFont($this->fontName, '', 11, '', false);
         $folhaDeRosto->Ln(5);
         $folhaDeRosto->Write(0, $this->tradutor->traduzir('metadata.property.displayName.doi', $this->locale) . ": " . $this->submissão->obterDOI(), '', 0, 'C', true, 0, false, false, 0);
         $folhaDeRosto->Ln(10);
         $folhaDeRosto->Write(0, $this->tradutor->traduzir('plugins.generic.folhaDeRostoDoPDF.rotuloDaChecklist', $this->locale) . ": ", '', 0, 'JUSTIFY', true, 0, false, false, 0);
-        $folhaDeRosto->SetFont($fontname, '', 10, '', false);
+        $folhaDeRosto->SetFont($this->fontName, '', 10, '', false);
         $folhaDeRosto->Ln(5);
 
         $textoChecklist = '';
@@ -55,7 +56,7 @@ class FolhaDeRosto {
             $textoChecklist = $textoChecklist. "<ul style=\"text-align:justify;\"><li>". $item . "</li></ul>";
         }
         $folhaDeRosto->writeHTMLCell(0, 0, '', '',$textoChecklist, 1, 1, false, true, 'JUSTIFY', false);
-        $folhaDeRosto->SetFont($fontname, '', 11, '', false);
+        $folhaDeRosto->SetFont($this->fontName, '', 11, '', false);
         $folhaDeRosto->Ln(5);
         $folhaDeRosto->Write(0, $this->tradutor->traduzir('plugins.geric.folhaDeRostoDoPDF.dataSubmissao', $this->locale) . ": " . $this->tradutor->obterDataTraduzida($this->submissão->obterDataDeSubmissão(), $this->locale), '', 0, 'JUSTIFY', true, 0, false, false, 0);
         $folhaDeRosto->Write(0, $this->tradutor->traduzir('plugins.geric.folhaDeRostoDoPDF.dataPublicacao', $this->locale) . ": " . $this->tradutor->obterDataTraduzida($this->submissão->obterDataDePublicação(), $this->locale), '', 0, 'JUSTIFY', true, 0, false, false, 0);
@@ -133,7 +134,6 @@ class FolhaDeRosto {
 
     private function adicionaHeaderPagina($caminhoPagina) {
         $pdf = new TCPDI(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
-        $fontName = TCPDF_FONTS::addTTFfont(__DIR__.'/../recursos/opensans.ttf', 'TrueTypeUnicode', '', 32);
 
         $pdf->SetPrintHeader(false);
         $pdf->AddPage();
@@ -142,7 +142,7 @@ class FolhaDeRosto {
         $pdf->useTemplate($tplIdx);
 
         $pdf->SetY(1);
-        $pdf->SetFont($fontName, '', 8);
+        $pdf->SetFont($this->fontName, '', 8);
         $pdf->Write(0, $this->obterTextoHeader(), '', 0, 'C', true, 0, false, false, 0);
 
         $caminhoSaida = self::DIRETORIO_DE_SAIDA . "paginaHeader";
