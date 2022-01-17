c78c2dca2b6074cd32c6d7b27bcd341f478cee7b
Jhon <jhon@lepidus.com.br>
2022-01-17 11:44:15 -0400

    Deletes deprecated script directory
    Issue: documentacao-e-tarefas/scielo#277
    
    Signed-off-by: Jhon <jhon@lepidus.com.br>

--- scripts/pre-commit.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Calling pre-commit hook..."
-./scripts/run-tests.sh
-
-if [ $? -ne 0 ]; then
-    echo "-------All tests must pass before the commit!-------"
-    exit 1
-fi
--- scripts/run-tests.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-testsCommand="phpunit --bootstrap classes/autoload.inc.php tests/"
-
-echo "Running unit tests..."
-eval "$testsCommand"
