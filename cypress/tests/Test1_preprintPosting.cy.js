6b50504cf032cf0c39a83474458ddc8798049df1
Jhon <jhon@lepidus.com.br>
2024-01-25 18:15:49 -0400

    Adds integration test to update title page on relation changing
    Issue: documentacao-e-tarefas/scielo#627
    
    Signed-off-by: Jhon <jhon@lepidus.com.br>
    Signed-off-by: Iudi <iudi@lepidus.com.br>

--- cypress/tests/Test1_preprintPosting.cy.js
+++ cypress/tests/Test1_preprintPosting.cy.js
@@ -1,3 +1,5 @@
+import '../support/commands.js';
+
 function beginSubmission(submissionData) {
     cy.get('input[name="locale"][value="en"]').click();
     cy.setTinyMceContent('startSubmission-title-control', submissionData.title);
@@ -37,54 +39,6 @@ function contributorsStep(submissionData) {
     cy.contains('button', 'Continue').click();
 }
 
-function downloadPdfFile(pdfUrl, directory, pdfFile, txtFile) {
-    cy.exec('mkdir ' + directory).its('code').should('eq', 0);
-    cy.exec('curl -o ' + pdfFile + ' ' + pdfUrl).its('code').should('eq', 0);
-    cy.exec('pdftotext ' + pdfFile + ' ' + txtFile).its('code').should('eq', 0);
-}
-
-function assertNumberPdfPages(pdfFile, expectedNumberOfPages) {
-    cy.exec('cpdf -pages ' + pdfFile).its('stdout').should('contain', expectedNumberOfPages);
-}
-
-function checkTitlePage(txtFile, submissionData) {
-    cy.exec("grep '" + submissionData.title + "' " + txtFile).its('code').should('eq', 0);
-    cy.exec("grep 'Publication status: This preprint has not been published elsewhere' " + txtFile).its('code').should('eq', 0);
-    
-    let authorFullName = submissionData.contributors[0]['given'] + ' '  + submissionData.contributors[0]['family'];
-    cy.exec("grep '" + authorFullName + "' " + txtFile).its('code').should('eq', 0);
-    
-    let today = (new Date()).toISOString().split('T')[0];
-    cy.exec("grep 'Submitted on: " + today + "' " + txtFile).its('code').should('eq', 0);
-    cy.exec("grep 'Posted on: " + today + "' " + txtFile).its('code').should('eq', 0);
-
-    cy.exec("grep 'This document is a preprint and its current status is available at:' " + txtFile).its('code').should('eq', 0);
-}
-
-function checkChecklistPage(txtFile) {
-    cy.exec("grep 'This preprint was submitted under the following conditions:' " + txtFile).its('code').should('eq', 0);
-    
-    cy.exec("grep 'This submission meets the requirements outlined in the Author Guidelines.' " + txtFile).its('code').should('eq', 0);
-    cy.exec("grep 'This submission has not been previously posted.' " + txtFile).its('code').should('eq', 0);
-    cy.exec("grep 'All references have been checked for accuracy and completeness.' " + txtFile).its('code').should('eq', 0);
-    cy.exec("grep 'All tables and figures have been numbered and labeled.' " + txtFile).its('code').should('eq', 0);
-}
-
-function performTitlePageCheckings(pdfUrl, submissionData) {
-    const directory = './plugins/generic/titlePageForPreprint/cypress/tests/result/';
-    const pdfFile = directory + 'document.pdf';
-    const txtFile = directory + 'document.txt';
-    const expectedNumberOfPages = 3;
-    
-    downloadPdfFile(pdfUrl, directory, pdfFile, txtFile);
-
-    assertNumberPdfPages(pdfFile, expectedNumberOfPages);
-    checkTitlePage(txtFile, submissionData);
-    checkChecklistPage(txtFile);
-
-    cy.exec('rm -r ' + directory).its('code').should('eq', 0);
-}
-
 describe('Title Page for Preprint Plugin - Title page stamping on preprint posting', function() {
     let submissionData;
     
@@ -94,6 +48,7 @@ describe('Title Page for Preprint Plugin - Title page stamping on preprint posti
             title: "A Nightmare on Elm Street",
 			abstract: 'Teenagers start to dream with a creepy man',
 			keywords: ['plugin', 'testing'],
+            relations: 1,
             contributors: [
                 {
                     'given': 'Wes',
@@ -113,7 +68,7 @@ describe('Title Page for Preprint Plugin - Title page stamping on preprint posti
 		};
     });
 
-    /*it('Author creates new submission with galley', function() {
+    it('Author creates new submission with galley', function() {
         cy.login('eostrom', null, 'publicknowledge');
         cy.get('div#myQueue a:contains("New Submission")').click();
 
@@ -141,9 +96,9 @@ describe('Title Page for Preprint Plugin - Title page stamping on preprint posti
         cy.contains('a', 'View').click();
         cy.contains('a', 'PDF').click();
         cy.get('a.download').invoke('attr', 'href').then(pdfUrl => {
-            performTitlePageCheckings(pdfUrl, submissionData);
+            cy.performTitlePageCheckings(submissionData, pdfUrl);
         });
-    });*/
+    });
     it('Title page updating', function () {
         cy.findSubmissionAsEditor('dbarnes', null, 'Ostrom');
         cy.get('#publication-button').click();
@@ -162,7 +117,7 @@ describe('Title Page for Preprint Plugin - Title page stamping on preprint posti
         cy.contains('a', 'View').click();
         cy.contains('a', 'PDF').click();
         cy.get('a.download').invoke('attr', 'href').then(pdfUrl => {
-            performTitlePageCheckings(pdfUrl, submissionData);
+            cy.performTitlePageCheckings(submissionData, pdfUrl);
         });
     });
 });
--- cypress/tests/Test2_relationChanging.cy.js
+++ cypress/tests/Test2_relationChanging.cy.js
@@ -1,66 +1,50 @@
-import 'core-js/modules/es.regexp.exec';
+import '../support/commands.js';
 
-function createDirectory(directory) {
-    let commandLineForCreateDirectory = 'mkdir ' + directory;
-    cy.exec(commandLineForCreateDirectory).its('code').should('eq', 0);
-}
-
-function downloadFile(receivedFile, url) {
-    let commandLineForDownload = 'curl -o ' + receivedFile + url;
-    cy.exec(commandLineForDownload).its('code').should('eq', 0);
-}
-
-function changeOnePageOfPdfToText(receivedFile, textFile) {
-    let CommandLineForPdfToText = 'pdftotext -f 1 -l 1 ' + receivedFile + textFile;
-    cy.exec(CommandLineForPdfToText).its('code').should('eq', 0);
-}
-
-function removeDirectory(directory) { 
-    let CommandLineForRemoveDirectory = 'rm -R ' + directory;
-    cy.exec(CommandLineForRemoveDirectory).its('code').should('eq', 0);
-}
-
-function checkRelationAfterChoice(textFile) { 
-    cy.readFile(textFile).should('contain','Preprint has been submitted for publication in journal');
-}
-
-describe('Title Page Plugin relation test', function() {
-    it('Change relation tests', function() {
-        cy.visit(Cypress.env('baseUrl') + 'index.php/f/submissions');
-        cy.get('input[id=username]').click();
-        cy.get('input[id=username]').type(Cypress.env('OPSAdminUsername'), { delay: 0 });
-        cy.get('input[id=password]').click();
-        cy.get('input[id=password]').type(Cypress.env('OPSAdminPassword'), { delay: 0 });
-        cy.get('button[class=submit]').click();
-
-        cy.get('#archive-button').click();
-        cy.get('.listPanel__itemActions > .pkpButton').click();
+describe('Title Page for Preprint Plugin - Title page update on relation changing', function() {
+    let submissionData;
+    
+    before(function() {
+        Cypress.config('defaultCommandTimeout', 4000);
+        submissionData = {
+            title: "A Nightmare on Elm Street",
+            relations: 3,
+            contributors: [
+                {
+                    'given': 'Wes',
+                    'family': 'Craven',
+                    'email': 'wes.craven@stab.com',
+                    'country': 'United States'
+                }
+            ]
+        };
+    });
+    
+    it('Moderator changes submission relations after it has been posted', function() {
+        cy.findSubmissionAsEditor('dbarnes', null, 'Ostrom');
         cy.get('#publication-button').click();
-        cy.get('.pkpPublication__relation > .pkpDropdown > .pkpButton').click();
 
-        cy.get('input[value^=2]').check();
-        cy.get('button[class=pkpButton]').contains('Save').click();
-        cy.contains('Save');
+        cy.get('#publication-button').click();
+		cy.get('.pkpHeader__actions button:contains("Unpost")').click();
+        cy.get('.modal__panel button:contains("Unpost")').click();
+        
+        cy.setTinyMceContent('titleAbstract-title-control-en', submissionData.title);
+        cy.get('#titleAbstract button:contains("Save")').click();
+        cy.waitJQuery();
 
-        cy.get('a.pkpButton').click();
-        cy.get('.obj_galley_link').click();
-        cy.get('.download').then((anchor) => {
-            const url = anchor.attr('href');
-            const directory = './plugins/generic/titlePageForPreprint/cypress/tests/result/';
-            const receivedFile = directory +'receivedFile.pdf ';
-            const textFile = directory + 'titlePage.txt';
+        cy.get('.pkpHeader__actions button:contains("Post")').click();
+        cy.get('.pkp_modal_panel button:contains("Post")').click();
+        cy.contains('span', 'Posted');
 
-            createDirectory(directory);
+        cy.contains('button', 'Relations').click();
+        cy.get('input[name="relationStatus"][value="3"]').check();
+        cy.get('.pkpWorkflow__relateForm button:contains("Save")').click();
+        cy.waitJQuery();
 
-            downloadFile(receivedFile, url);
-            
-            changeOnePageOfPdfToText(receivedFile, textFile);
-
-            checkRelationAfterChoice(textFile);
-
-            removeDirectory(directory);
+        cy.contains('a', 'View').click();
+        cy.contains('a', 'PDF').click();
+        cy.get('a.download').invoke('attr', 'href').then(pdfUrl => {
+            cy.performTitlePageCheckings(submissionData, pdfUrl);
         });
-
     });
 })
 
