d53ffad3b7ada05f204dd243e542e6664d84f4d0
Jhon <jhon@lepidus.com.br>
2024-01-25 15:59:31 -0400

    Integration test checks title page stamping
    Issue: documentacao-e-tarefas/scielo#627
    
    Signed-off-by: Jhon <jhon@lepidus.com.br>
    Signed-off-by: Iudi <iudi@lepidus.com.br>

--- cypress/tests/Test1_preprintPosting.cy.js
+++ cypress/tests/Test1_preprintPosting.cy.js
@@ -37,6 +37,39 @@ function contributorsStep(submissionData) {
     cy.contains('button', 'Continue').click();
 }
 
+function downloadPdfFile(pdfUrl, directory, pdfFile, txtFile) {
+    cy.exec('mkdir ' + directory).its('code').should('eq', 0);
+    cy.exec('curl -o ' + pdfFile + ' ' + pdfUrl).its('code').should('eq', 0);
+    cy.exec('pdftotext ' + pdfFile + ' ' + txtFile).its('code').should('eq', 0);
+}
+
+function assertNumberPdfPages(pdfFile, expectedNumberOfPages) {
+    cy.exec('cpdf -pages ' + pdfFile).its('stdout').should('contain', expectedNumberOfPages);
+}
+
+function checkTitlePage(txtFile, submissionData) {
+    cy.exec("grep '" + submissionData.title + "' " + txtFile).its('code').should('eq', 0);
+    cy.exec("grep 'Publication status: This preprint has not been published elsewhere' " + txtFile).its('code').should('eq', 0);
+    
+    let authorFullName = submissionData.contributors[0]['given'] + ' '  + submissionData.contributors[0]['family'];
+    cy.exec("grep '" + authorFullName + "' " + txtFile).its('code').should('eq', 0);
+    
+    let today = (new Date()).toISOString().split('T')[0];
+    cy.exec("grep 'Submitted on: " + today + "' " + txtFile).its('code').should('eq', 0);
+    cy.exec("grep 'Posted on: " + today + "' " + txtFile).its('code').should('eq', 0);
+
+    cy.exec("grep 'This document is a preprint and its current status is available at:' " + txtFile).its('code').should('eq', 0);
+}
+
+function checkChecklistPage() {
+    cy.exec("grep 'This preprint was submitted under the following conditions:' " + txtFile).its('code').should('eq', 0);
+    
+    cy.exec("grep 'This submission meets the requirements outlined in the Author Guidelines.' " + txtFile).its('code').should('eq', 0);
+    cy.exec("grep 'This submission has not been previously posted.' " + txtFile).its('code').should('eq', 0);
+    cy.exec("grep 'All references have been checked for accuracy and completeness.' " + txtFile).its('code').should('eq', 0);
+    cy.exec("grep 'All tables and figures have been numbered and labeled.' " + txtFile).its('code').should('eq', 0);
+}
+
 describe('Title Page for Preprint Plugin - Title page stamping on preprint posting', function() {
     let submissionData;
     
@@ -90,7 +123,22 @@ describe('Title Page for Preprint Plugin - Title page stamping on preprint posti
         cy.get('.pkp_modal_panel button:contains("Post")').click();
         cy.contains('span', 'Posted');
         
-        //verificar que adicionou folha de rosto e página da checklist
+        cy.contains('a', 'View').click();
+        cy.contains('a', 'PDF').click();
+        cy.get('a.download').invoke('attr', 'href').then(pdfUrl => {
+            const directory = './plugins/generic/titlePageForPreprint/cypress/tests/result/';
+            const pdfFile = directory + 'document.pdf';
+            const txtFile = directory + 'document.txt';
+            const expectedNumberOfPages = 3;
+            
+            downloadPdfFile(pdfUrl, directory, pdfFile, txtFile);
+
+            assertNumberPdfPages(pdfFile, expectedNumberOfPages);
+            checkTitlePage(txtFile, submissionData);
+            checkChecklistPage(txtFile);
+
+            cy.exec('rm -r ' + directory).its('code').should('eq', 0);
+        });
     });
 });
 
