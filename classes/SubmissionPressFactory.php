94bec52c4b6a12a1814a2a9887aafd9206f587a6
Jhon <jhon@lepidus.com.br>
2025-02-04 15:13:13 -0400

    Removes last mentions to data statement in plugin
    Issue: documentacao-e-tarefas/scielo#741
    
    Signed-off-by: Jhon <jhon@lepidus.com.br>

--- classes/SubmissionPressFactory.php
+++ classes/SubmissionPressFactory.php
@@ -92,10 +92,6 @@ class SubmissionPressFactory
         $data['isTranslation'] = !is_null($publication->getData('originalDocumentDoi'));
         $data['citation'] = ($data['isTranslation'] ? $this->getSubmissionCitation($submission) : '');
 
-        if ($publication->getData('dataStatementTypes')) {
-            $data['dataStatement'] = $this->getDataStatement($publication);
-        }
-
         $data['endorserName'] = $publication->getData('endorserName');
         $data['endorserOrcid'] = $publication->getData('endorserOrcid');
 
@@ -114,41 +110,5 @@ class SubmissionPressFactory
         $citation = $cslPlugin->getCitation($request, $submission, 'apa');
 
         return $citation;
-    }
-
-    private function getDataStatement($publication)
-    {
-        $dataStatementService = new \APP\plugins\generic\dataverse\classes\services\DataStatementService();
-        $dataStatementTypes = [
-            $dataStatementService::DATA_STATEMENT_TYPE_IN_MANUSCRIPT => 'plugins.generic.dataverse.dataStatement.inManuscript',
-            $dataStatementService::DATA_STATEMENT_TYPE_REPO_AVAILABLE => 'plugins.generic.dataverse.dataStatement.repoAvailable',
-            $dataStatementService::DATA_STATEMENT_TYPE_ON_DEMAND => 'plugins.generic.dataverse.dataStatement.onDemand',
-            $dataStatementService::DATA_STATEMENT_TYPE_PUBLICLY_UNAVAILABLE => 'plugins.generic.dataverse.dataStatement.publiclyUnavailable'
-        ];
-        $dataStatement = [];
-
-        foreach ($publication->getData('dataStatementTypes') as $selectedStatement) {
-            if ($selectedStatement == $dataStatementService::DATA_STATEMENT_TYPE_DATAVERSE_SUBMITTED) {
-                continue;
-            }
-
-            $dataStatement[$selectedStatement] = $dataStatementTypes[$selectedStatement];
-
-            if ($selectedStatement == $dataStatementService::DATA_STATEMENT_TYPE_REPO_AVAILABLE) {
-                $dataStatement[$selectedStatement] = [
-                    'message' => $dataStatementTypes[$selectedStatement],
-                    'dataStatementUrls' => $publication->getData('dataStatementUrls')
-                ];
-            }
-
-            if ($selectedStatement == $dataStatementService::DATA_STATEMENT_TYPE_PUBLICLY_UNAVAILABLE) {
-                $dataStatement[$selectedStatement] = [
-                    'message' => $dataStatementTypes[$selectedStatement],
-                    'dataStatementReason' => $publication->getLocalizedData('dataStatementReason')
-                ];
-            }
-        }
-
-        return $dataStatement;
     }
 }
